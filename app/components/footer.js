65bb0ca784c9efe709ba5b0fdf4074ef001c1082
ojizou003 <sinissi@gmail.com>
2024-09-19 15:14:22 +0900

    ♻️ 繰り返しのリファクタリング

--- app/components/footer.js
+++ app/components/footer.js
@@ -1,53 +1,49 @@
 import Image from "next/image";
 
+const ITEMS = [
+  {
+  href: 'https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app',
+  src: 'https://nextjs.org/icons/file.svg',
+  alt: 'File icon',
+  text: 'Learn'
+}, 
+{
+  href: 'https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app',
+  src: 'https://nextjs.org/icons/window.svg',
+  alt: 'Window icon',
+  text: 'Examples'
+}, 
+{
+  href: 'https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app',
+  src: 'https://nextjs.org/icons/globe.svg',
+  alt: 'Globe icon',
+  text: 'Go to nextjs.org →'
+}, 
+]
+
 export function Footer() {
   return (
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
+        {ITEMS.map(item => {
+          return (
+            <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href={item.href}
           target="_blank"
           rel="noopener noreferrer"
         >
           <Image
             aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
+            src={item.src}
+            alt={item.alt}
             width={16}
             height={16}
           />
-          Learn
+          {item.text}
         </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+          );
+        })}
+        
       </footer>
   );
 }
