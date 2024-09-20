8f73dba49cc28a259dd76a75815ee8afb649b703
ojizou003 <sinissi@gmail.com>
2024-09-20 10:32:04 +0900

    ✨ useStateの導入

--- app/page.jsx
+++ app/page.jsx
@@ -4,16 +4,15 @@ import { Header } from './components/header';
 import { Headline } from './components/headline';
 import { Links } from './components/links';
 import { Footer } from './components/footer';
-import { useCallback, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
-  const foo = 1;
 
-  const handleClick = useCallback((e) => {
-    console.log(e.target.href);
-    e.preventDefault();
-    alert(foo);
-  }, []);
+  const [count, setCount] = useState(1);
+
+  const handleClick = (e) => {
+    setCount(count => count+1);
+  };
   
   useEffect(() => {
     document.body.style.backgroundColor = 'lightblue';
@@ -22,19 +21,15 @@ export default function Home() {
 
   return (
     <body>
-          <Header />
+      <Header />
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        <a 
-          href='/about'
-          onClick={handleClick}
-        >
-          ボタン
-        </a>
+        <button onClick={handleClick}>ボタン</button>
         <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
           <Headline page='Home' />
           <Links />
+          <h1 >{count}</h1>
         </main>
-        <Footer />
+      <Footer />
       </div>
     </body>
   );
